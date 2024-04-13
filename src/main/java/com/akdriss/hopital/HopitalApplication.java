78dbc9601bc01be7a6958fae0e5fee2f39ec2dbc
Driss <drissaitkassi@gmail.com>
2024-04-13 22:02:43 +0000

    tp part 2 finished

--- src/main/java/com/akdriss/hopital/HopitalApplication.java
+++ src/main/java/com/akdriss/hopital/HopitalApplication.java
@@ -24,10 +24,10 @@ public class HopitalApplication implements CommandLineRunner {
         Patient p1=new Patient(null,"mohamed",new Date(),false,23);
         Patient p2=new Patient(null,"ahmed",new Date(),true,33);
         Patient p3=new Patient(null,"hassan",new Date(),false,43);
-
+/*
         patientRepository.save(p1);
         patientRepository.save(p2);
-        patientRepository.save(p3);
+        patientRepository.save(p3);*/
 
 
 
--- src/main/java/com/akdriss/hopital/web/PatientContoller.java
+++ src/main/java/com/akdriss/hopital/web/PatientContoller.java
@@ -2,12 +2,16 @@ package com.akdriss.hopital.web;
 
 
 import com.akdriss.hopital.entities.Patient;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -29,6 +33,57 @@ public class PatientContoller {
         model.addAttribute("keyword",kw);
 
         return "patients";
+    }
+
+    @GetMapping("formPatient")
+    public String addPatient(Model model
+
+    ){
+
+        model.addAttribute("patient",new Patient());
+        return "formPatient";
+    }
+
+    @GetMapping("editPatient")
+    public String editPatient(Model model,
+                              @RequestParam(name = "id") Long id,
+                              @RequestParam(name = "page" , defaultValue = "0") int  page,
+                              @RequestParam(name = "size" ,defaultValue = "10")int size,
+                              @RequestParam(name = "keyword" ,defaultValue = "")String kw
+
+    ){
+
+       Patient patient= patientRepository.findById(id).orElse(null);
+       if (patient == null)throw new RuntimeException("patient introuvable");
+       model.addAttribute("patient",patient);
+        //todo add attributes for /save
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",kw);
+        return "editPatient";
+    }
+
+    @PostMapping("edit")
+    public String updatePatient(Model model, @Valid Patient patient, BindingResult bindingResult
+    ){
+        if (bindingResult.hasErrors())return "editPatient";
+        patientRepository.save(patient);
+
+        return "redirect:editPatient";
+    }
+
+    @PostMapping("save")
+    public String savePatient(Model model, @Valid Patient patient, BindingResult bindingResult,
+
+                              @RequestParam(name = "page" , defaultValue = "0") int  page,
+                              @RequestParam(name = "size" ,defaultValue = "10")int size,
+                              @RequestParam(name = "keyword" ,defaultValue = "")String kw
+                              ){
+        if (bindingResult.hasErrors())return "formPatient";
+        patientRepository.save(patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",kw);
+
+        return "redirect:/index?page="+page+"&keyword="+kw;
     }
     @GetMapping("delete")
     public String delete(Model model,
